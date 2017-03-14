12fe404f85fad9cde25c7d88d18f5d819b131d68
vadim-bashynskiy <underklon@mail.ru>
2017-03-14 11:57:39 +0200

    Add Lesson3 : class Runner and Convertors

---
+++ src/main/java/com/bashynskiy/app/utils/runners/Lesson3Runner.java
@@ -0,0 +1,15 @@
+package com.bashynskiy.app.utils.runners;
+
+import com.bashynskiy.app.utils.convertors.PrimitiveConvertor;
+
+/**
+ * Created by testu on 3/14/2017.
+ */
+public class Lesson3Runner {
+    public static void main(String[] args) {
+        PrimitiveConvertor convertor = new PrimitiveConvertor();
+        convertor.floatToChar(71.6f);
+        convertor.charToInt('i');
+        convertor.intToChar(116);
+    }
+}
--- src/test/java/com/bashynskiy/AppTest.java
+++ src/test/java/com/bashynskiy/AppTest.java
@@ -1,4 +1,4 @@
-package com.vadim-bashynskiy.app;
+package com.bashynskiy;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
