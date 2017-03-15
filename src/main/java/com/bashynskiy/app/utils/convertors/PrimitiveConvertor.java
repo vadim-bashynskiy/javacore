4e69306e6f4ed22efc93a86962a85526dec39210
vadim-bashynskiy <underklon@mail.ru>
2017-03-15 20:30:09 +0300

    some space

--- src/main/java/com/bashynskiy/app/utils/convertors/PrimitiveConvertor.java
+++ src/main/java/com/bashynskiy/app/utils/convertors/PrimitiveConvertor.java
@@ -11,12 +11,12 @@ public class PrimitiveConvertor {
 
     public void floatToChar(float float1) {
         char1 = (char) float1;
-        System.out.println("Input float value is "+ float1 + " Output char value is "+ char1);
+        System.out.println("Input float value is " + float1 + " Output char value is "+ char1);
     }
 
     public void intToChar(int int1) {
         char1 = (char) int1;
-        System.out.println("Input float value is " + int1 +" Output char value is"+char1);
+        System.out.println("Input float value is " + int1 + " Output char value is "+ char1);
     }
 
     public void charToInt(char char1) {
