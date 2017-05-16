f5faba674bbb7bc5578987baad6c348985857086
Vadym Bashynskyi <vadym.bashynskyi@betbull.com>
2017-05-16 18:05:01 +0300

    homework15 add part2

---
+++ src/main/java/com/bashynskiy/app/homework/lesson15/Flat.java
@@ -0,0 +1,22 @@
+package com.bashynskiy.app.homework.lesson15;
+
+/**
+ * Created by VBashynskyi on 16.05.2017.
+ */
+public class Flat {
+
+    private String tvName;
+    private String dishes;
+
+    public String getTvName() {
+        return tvName;
+    }
+
+    public void setTvName(String tvName) {
+        this.tvName = tvName;
+    }
+
+    public String getDishes() {
+        return dishes;
+    }
+}
---
+++ src/main/java/com/bashynskiy/app/homework/lesson15/House.java
@@ -0,0 +1,7 @@
+package com.bashynskiy.app.homework.lesson15;
+
+/**
+ * Created by VBashynskyi on 16.05.2017.
+ */
+public class House {
+}
