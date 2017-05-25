738bd1208622b247f3c64b098521fe8dbd926d6d
Vadym Bashynskyi <vadym.bashynskyi@betbull.com>
2017-05-25 18:11:33 +0300

    homework18 generics

---
+++ src/main/java/com/bashynskiy/app/homework/lesson18/ArraySorterGenerics.java
@@ -0,0 +1,28 @@
+package com.bashynskiy.app.homework.lesson18;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by VBashynskyi on 25.05.2017.
+ */
+public class ArraySorterGenerics <T> {
+
+ /*   private List<T> arrayList = new ArrayList<>();
+
+    public ArraySorterGenerics(List<T>  arrayList) {
+        this.arrayList = arrayList;
+    }
+    public void sort(){
+        return arrayList.sort();
+
+    }
+
+    public List<T> getArrayList() {
+        return arrayList;
+    }
+
+    public static void main(String[] args) {
+        ArraySorterGenerics arraySorterGenerics = new ArraySorterGenerics();
+    }*/
+}
