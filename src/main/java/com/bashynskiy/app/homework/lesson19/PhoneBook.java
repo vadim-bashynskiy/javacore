4d3784a2054cfb0b1cb7d7cfacfae4019ae376ad
Vadym Bashynskyi <vadym.bashynskyi@betbull.com>
2017-05-30 18:01:29 +0300

    homework19

--- src/main/java/com/bashynskiy/app/homework/lesson19/PhoneBook.java
+++ src/main/java/com/bashynskiy/app/homework/lesson19/PhoneBook.java
@@ -48,10 +48,4 @@ public class PhoneBook {
         }
         return false;
     }
-
-    public static void main(String[] args) {
-        Menu menu = new Menu();
-        menu.printMenu();
-
-    }
 }
---
+++ src/main/java/com/bashynskiy/runners/homework/lesson19/PhoneBookRunner.java
@@ -0,0 +1,14 @@
+package com.bashynskiy.runners.homework.lesson19;
+
+import com.bashynskiy.app.homework.lesson19.Menu;
+
+/**
+ * Created by VBashynskyi on 30.05.2017.
+ */
+public class PhoneBookRunner {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.printMenu();
+
+    }
+}
